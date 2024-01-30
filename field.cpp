d9748f4f48f5a907549069dda981fc7eb1731a58
Joellebo <joellebo@student.21-school.ru>
2024-01-31 00:30:52 +0700

    too long

--- field.cpp
+++ field.cpp
@@ -1,40 +1,46 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
+#include <stack>
 #include <string>
 #include <tuple>
 #include <vector>
 
 using Coord = std::pair<int, int>;
-using Boxes = std::vector<std::tuple<Coord, Coord, int, int>>;
+using Boxes = std::vector<std::tuple<Coord, Coord, int>>;
 
-void parse_field(const std::vector<std::string> &field, Boxes &boxes,
-                 Coord start, Coord end, int level) {
+void parse_field(std::vector<std::string> &field, Boxes &boxes, Coord start,
+                 Coord end, int level) {
   Coord top_left, down_rigth;
   if (level > 0) {
-    start.first += 1;
-    start.second += 1;
+    start.first += 2;
+    start.second += 2;
     end.first -= 1;
     end.second -= 1;
   }
+
   for (int i = start.first; i < end.first; i++) {
     for (int j = start.second; j < end.second; j++) {
       if (field[i][j] == '*' && (i == 0 || field[i - 1][j] == '.') &&
           (j == 0 || field[i][j - 1] == '.')) {
         top_left = {i, j};
-        if (std::find_if(boxes.begin(), boxes.end(), [top_left](auto box) {
-              return std::get<0>(box) == top_left;
-            }) != boxes.end()) {
-          continue;
-        }
+        // auto box = std::find_if(
+        //     boxes.begin(), boxes.end(),
+        //     [top_left](auto box) { return std::get<0>(box) == top_left; });
+        // if (box != boxes.end()) {
+        //   j = std::get<1>(*box).second + 1;
+        //   continue;
+        // }
+        i += 3;
         while (i < end.first && field[i][j] == '*') i++;
+        j += 3;
         while (j < end.second && field[i - 1][j] == '*') j++;
-        down_rigth = {i - (i == end.first ? 0 : 1),
-                      j - (j == end.second ? 0 : 1)};
-        boxes.emplace_back(top_left, down_rigth,
-                           (down_rigth.first - top_left.first + 1) *
-                               (down_rigth.second - top_left.second + 1),
-                           level);
-        parse_field(field, boxes, top_left, down_rigth, level + 1);
+        down_rigth = {i - 1, j - 1};
+        boxes.emplace_back(top_left, down_rigth, level);
+        if (down_rigth.first - top_left.first > 5 &&
+            down_rigth.second - top_left.second > 5)
+          parse_field(field, boxes, top_left, down_rigth, level + 1);
+        field[top_left.first][top_left.second] = '0';  // mark visited
         i = top_left.first;
         j = down_rigth.second;
         continue;
@@ -60,9 +66,9 @@ int main(void) {
   for (int i = 0; i < t; i++) {
     std::vector<int> res;
     std::sort(boxes[i].begin(), boxes[i].end(),
-              [](auto a, auto b) { return std::get<3>(a) < std::get<3>(b); });
+              [](auto a, auto b) { return std::get<2>(a) < std::get<2>(b); });
     for (size_t j = 0; j < boxes[i].size(); j++) {
-      std::cout << std::get<3>(boxes[i][j]) << " ";
+      std::cout << std::get<2>(boxes[i][j]) << " ";
     }
     std::cout << std::endl;
   }
