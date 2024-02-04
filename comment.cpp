4c9e5c5b3551bbbf56e9b02fffee7d7457321fa1
Joellebo <joellebo@student.21-school.ru>
2024-02-04 13:49:23 +0700

    comments on vector before map

--- comment.cpp
+++ comment.cpp
@@ -5,17 +5,31 @@
 #include <utility>
 #include <vector>
 
-using Comments = std::map<int, std::pair<int, std::string>>;
-
 struct Comment {
   int parent_id = -1;
   int id = 0;
-  std::set<Comment> childs;
+  std::string text;
+  // std::set<Comment> childs{};
 };
 
-struct CommentTree {
-  Comment root;
-};
+// using Comments = std::map<int, std::vector<Comment>>;
+using Comments = std::map<int, std::vector<Comment>>;
+
+void PrintPrefix(int level) {
+  if (level == 0) return;
+  std::cout << "|\n";
+  std::cout << "|";
+  for (int i = 0; i < level; ++i) {
+    std::cout << "|--";
+  }
+}
+void PrintCommentTree(Comments& comments, int parent_id, int level) {
+  for (auto child : comments[parent_id]) {
+    PrintPrefix(level);
+    std::cout << child.text << std::endl;
+    PrintCommentTree(comments, child.id, level + 1);
+  }
+}
 
 int main(void) {
   int t, n;
@@ -26,10 +40,13 @@ int main(void) {
     for (int j = 0; j < n; j++) {
       int id, p;
       std::string text;
-      std::cin >> id >> p >> text;
-      comments[i][id] = std::make_pair(p, text);
+      std::cin >> id >> p;
+      std::cin.ignore();
+      std::getline(std::cin, text);
+      comments[i][p].push_back({p, id, text});
     }
   }
   for (int i = 0; i < t; i++) {
+    PrintCommentTree(comments[i], -1, 0);
   }
 }
